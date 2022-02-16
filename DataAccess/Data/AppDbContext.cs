261e6909028a66d94ecc4e44716ca60a3c272dfc
Arvind Kumar Chaudhary <arvind-sandesh@outlook.com>
2022-02-16 18:15:31 +0530

    manage project class lib

---
+++ DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Shared.Models;
+
+namespace DataAccess.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
